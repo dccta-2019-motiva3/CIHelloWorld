713e3856a4b3d6e7a887b20cf888b8d48c04c1eb
Maximiliano Edwards <maximiliano.edwards@ug.uchile.cl>
2019-04-02 18:57:13 +0000

    tabs fixed

--- hello/tests.py
+++ hello/tests.py
@@ -3,19 +3,19 @@ from django.test import Client, TestCase
 #create your tests here
 
 class HelloWorldTest(TestCase):
-	
-  def test_reachable(self):
-	#given
-	client = Client()
+    
+    def test_reachable(self):
+	    #given
+	    client = Client()
 
-	#when 
-	response = client.get("/hello_world/")
+	    #when 
+	    response = client.get("/hello_world/")
 
-	#then 
-	self.assertEqual(200, response.status_code)
+	    #then 
+	    self.assertEqual(200, response.status_code)
 
-  def test_hello_world(self):
+    def test_hello_world(self):
 
-	client = Client()
-	response = client.get("/hello_world/")
-	self.assertIn("Hello world !", response.content)
+	    client = Client()
+	    response = client.get("/hello_world/")
+	    self.assertIn("Hello world !", response.content)
